b6598c80da216b517d868fd60d672e4f18dfa233
Junhyun Kim <linokim220@gmail.com>
2024-09-09 01:36:27 +0900

    refactor: protected router settings

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 import Home from "./Home";
 import Login from "./Login";
@@ -7,16 +12,52 @@ import TestPage from "./Testpage";
 import Signup from "./Signup";
 import TestResultPage from "./TestResultPage";
 
+const ProtectedRoute = ({ user, children }) => {
+  return user ? children : <Navigate to="/login" />;
+};
+
+const UnAuthProtectedRoute = ({ user, children }) => {
+  return !user ? children : <Navigate to="/" />;
+};
+
 const AppRouter = () => {
+  const user = true;
+
+  const protectedRoutes = [
+    { path: "/profile", element: <Profile /> },
+    { path: "/test", element: <TestPage /> },
+    { path: "/results", element: <TestResultPage /> },
+  ];
+
+  const unAuthProtectedRoutes = [
+    { path: "/login", element: <Login /> },
+    { path: "/signup", element: <Signup /> },
+  ];
+
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/test" element={<TestPage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/result" element={<TestResultPage />} />
+        {unAuthProtectedRoutes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={
+              <UnAuthProtectedRoute user={user}>
+                {route.element}
+              </UnAuthProtectedRoute>
+            }
+          />
+        ))}
+        {protectedRoutes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={
+              <ProtectedRoute user={user}>{route.element}</ProtectedRoute>
+            }
+          />
+        ))}
       </Routes>
     </Router>
   );
